798971771860a6d3b2b6b13938fdf4c0b5d559eb
Oscar Duarte <leodufer@gmail.com>
2017-08-23 21:13:41 -0400

    add login activity and dashboard activity

--- app/src/main/java/py/edu/facitec/quizz/LoginActivity.java
+++ app/src/main/java/py/edu/facitec/quizz/LoginActivity.java
@@ -1,56 +1,52 @@
 package py.edu.facitec.quizz;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
-
-import java.util.regex.Pattern;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText emailEditText;
     EditText passwordEditText;
-
+    int back=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         emailEditText = (EditText) findViewById(R.id.editTextEmail);
         passwordEditText = (EditText) findViewById(R.id.editTextPassword);
-
     }
 
     public void acceder(View view){
         String email = emailEditText.getText().toString();
-        String password = passwordEditText.getText().toString();
 
-
-        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            emailEditText.setError("Complete el email");
+        if(email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEditText.setError("Escriba un email valido");
+            return;
+        }
+        if(passwordEditText.getText().toString().isEmpty()){
+            passwordEditText.setError("Escriba su password");
             return;
         }
 
-        if(password.isEmpty()){
-            passwordEditText.setError("Complete el password");
-            return;
+        //TODO acceso de usuarios
+        if(emailEditText.getText().toString().equals("leodufer@gmail.com") && passwordEditText.getText().toString().equals("123")){
+            //Acceder a la app
+            Intent i = new Intent(LoginActivity.this,DashboardActivity.class);
+            startActivity(i);
+            finish();
+        }else{
+            //error de acceso
         }
-        //TODO login
-        Intent i = new Intent(this,MainActivity.class);
-        startActivity(i);
-        finish();
-
 
     }
 
     public void registrar(View view){
 
     }
-
-
 }
